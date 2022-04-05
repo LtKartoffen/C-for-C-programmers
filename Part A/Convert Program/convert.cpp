15c3b1464c31b0853b2a630972147bbfe5ab827e
Krasniuk Yurii <99223621+LtKartoffen@users.noreply.github.com>
2022-04-05 13:08:14 +0300

    added fill_array()

--- Part A/Convert Program/convert.cpp
+++ Part A/Convert Program/convert.cpp
@@ -6,20 +6,26 @@
 template <class summable>
 // Short func - we can use inline word
 // Setiing the func argument as a reference to type - convinient solution (C++ style)
-inline void sum(summable& p, int n, std::vector<summable> d)		// sum() will take reference to summable-type, size of array and object vector
-{									// and then will calculus sum of array and write one to variable p
-	p = 0;
-	for (int i = 0; i < n; i++)
-		p += d[i];
+inline void sum(summable& sum, int size, std::vector<summable> data)	// sum() will take reference to summable-type, size of array and object vector
+{																		// and then will calculus sum of array and write one to variable p
+	sum = 0;
+	for (int i = 0; i < size; i++)
+		sum += data[i];
+}
+// Inline func for fill numeric array by values
+template <class numeric>
+inline void fill_array(std::vector<numeric> data, int size)
+{
+	for (int i = 0; i < size; i++)					
+		data[i] = static_cast<numeric> (i);				// safety static cast in C++
 }
 int main()
 {
 	int accum;
-	const int N = 40;								// size for array
-	std::vector<int>data(N);
-	for (int i = 0; i < N; i++)							// fill array by values
-		data[i] = i;
-	sum(accum, N, data);								// culc sum in accum
-	std::cout << "sum is " << accum << std::endl;
+	const int SIZE = 40;								// size for array
+	std::vector<int>data(SIZE);							// creating vector object with 40 elements
+	fill_array(data, SIZE);				
+	sum(accum, SIZE, data);								// culc sum in accum
+	std::cout << "sum is " << accum << std::endl;		// out of result
 	return 0;
 }
