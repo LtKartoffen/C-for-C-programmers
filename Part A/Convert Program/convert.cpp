83f940d35c42dfc938b1da53c46acd06f722cc9b
Krasniuk Yurii <99223621+LtKartoffen@users.noreply.github.com>
2022-04-05 12:52:58 +0300

    added template and reference type

--- Part A/Convert Program/convert.cpp
+++ Part A/Convert Program/convert.cpp
@@ -1,21 +1,25 @@
 // convert.cpp -- converted version of C-program to C++ program
 #include <iostream>	// C++ library input and output
-#include <vector>	// std::vector objects
-// short func - we can use inline word
-inline void sum(int* p, int n, std::vector<int> d)
-{
-	*p = 0;
+#include <vector>	// std::vector objects - alternative to array
+
+// Generic idea - using template functions
+template <class summable>
+// Short func - we can use inline word
+// Setiing the func argument as a reference to type - convinient solution (C++ style)
+inline void sum(summable& p, int n, std::vector<summable> d)		// sum() will take reference to summable-type, size of array and object vector
+{									// and then will calculus sum of array and write one to variable p
+	p = 0;
 	for (int i = 0; i < n; i++)
-		*p += d[i];
+		p += d[i];
 }
 int main()
 {
 	int accum;
-	const int N = 40;
+	const int N = 40;								// size for array
 	std::vector<int>data(N);
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < N; i++)							// fill array by values
 		data[i] = i;
-	sum(&accum, N, data);
+	sum(accum, N, data);								// culc sum in accum
 	std::cout << "sum is " << accum << std::endl;
 	return 0;
 }
